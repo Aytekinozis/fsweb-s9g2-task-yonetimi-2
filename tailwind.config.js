8c69ad2259b7cd29ff64ce240e503906e4f6d0a1
Aytekinozis <aytekinozis93@gmail.com>
2023-12-15 17:58:21 +0300

    proje tamamlandi

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: { yeniMavi: "#d2d5fd" },
+    },
+  },
+  plugins: [],
+};
